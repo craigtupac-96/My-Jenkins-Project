83af1bcf8a2624bb1403c2204943a8aa173071e2
Craig Lawlor <craigtupac-96@users.noreply.github.com>
2019-01-07 11:41:36 +0000

    Added a line to Student.java

--- Student.java
+++ Student.java
@@ -3,6 +3,7 @@ public class Student {
     public enum AttendanceGrade {ABSENT, VERY_POOR, POOR, AVERAGE, GOOD, VERY_GOOD};
 
     public AttendanceGrade getAttendanceGrade(int attendance) {
+        if (attendance < 0 || attendance > 100) throw new IllegalArgumentException();
         if (attendance == 0) return AttendanceGrade.ABSENT;
         if (attendance < 30) return AttendanceGrade.VERY_POOR;
         if (attendance < 70) return AttendanceGrade.AVERAGE;
